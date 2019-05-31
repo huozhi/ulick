d66faf13d9049d6936e224527ae158fc08177fa1
huozhi <inbox@huozhi.im>
2019-06-01 02:08:10 +0800

    docs: hover example

--- docs/app.js
+++ docs/app.js
@@ -1,18 +1,19 @@
-import ulick from '../ulick.js'
-// 'https://cdn.jsdelivr.net/gh/huozhi/ulick@master/ulick.js'
+import ulick
+  from 'https://cdn.jsdelivr.net/gh/huozhi/ulick@master/ulick.js'
 
 const animatingClsSelector = 'touch__point--flicking'
+const fadingClsSelector = 'hover__pad--fading'
 const appNode = document.querySelector('#app')
 const touchPadNode = document.querySelector('.touch__pad')
+const hoverPadNode = document.querySelector('.hover__pad')
 
 function flicking(e) {
-  console.log('enter')
-  const point = e//(e.touches && e.touches.length) ? e.touches[0] : e
-  console.log('e.touches', point)
   const touchPoint = document.createElement('div')
-  touchPoint.style.top = point.pageY + 'px'
-  touchPoint.style.left = point.pageX + 'px'
+  touchPoint.style.top = e.pageY + 'px'
+  touchPoint.style.left = e.pageX + 'px'
   touchPoint.className = 'touch__point'
+  touchPoint.style.backgroundColor = (e instanceof window.MouseEvent) ? 'aquamarine' : 'orange'
+
   touchPadNode.appendChild(touchPoint)
 
   touchPoint.classList.add(animatingClsSelector)
@@ -21,11 +22,20 @@ function flicking(e) {
   })
 }
 
-ulick(appNode, {
-  onTouchClick(e) {
-    flicking(e)
-  },
-  onMouseClick(e) {
-    flicking(e)
+function fading(isIn) {
+  if (isIn) {
+    hoverPadNode.classList.add(fadingClsSelector)
+  } else {
+    hoverPadNode.classList.remove(fadingClsSelector)
   }
+}
+
+ulick(appNode, {
+  onTouchClick(e) { flicking(e) },
+  onMouseClick(e) { flicking(e) },
+})
+
+ulick(hoverPadNode, {
+  onHoverEnter(e) { fading(true) },
+  onHoverLeave(e) { fading(false) },
 })
